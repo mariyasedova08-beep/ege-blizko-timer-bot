e5166c3fb50960d581d29709ee7ad15e787efc0c
mariyasedova08-beep <mariyasedova08@gmail.com>
2026-09-11 13:07:56 +0300

    Remove temporary homework audit from startup

---
+++ run_bot_live58.py
@@ -0,0 +1,48 @@
+import run_bot_live56
+
+live56 = run_bot_live56
+live55 = live56.live55
+live54 = live55.live54
+live52 = live55.live52
+live51 = live55.live51
+live50 = live55.live50
+live49 = live55.live49
+live48 = live55.live48
+live46 = live55.live46
+live44 = live55.live44
+live43 = live55.live43
+live41 = live55.live41
+live39 = live55.live39
+live37 = live55.live37
+live35 = live55.live35
+live34 = live55.live34
+live31 = live55.live31
+live24 = live55.live24
+live17 = live55.live17
+bot = live55.bot
+
+
+if __name__ == "__main__":
+    live31.live25.ensure_lesson_day_before_table()
+    live31.live24.ensure_probnik_poll_tables()
+    live31.live28.ensure_unanswered_reminder_tables()
+    live31.live30.live3.ensure_attendance_tables()
+    live31.live30.ensure_auto_attendance_table()
+    live31.ensure_personal_homework_reminder_table()
+    live17.ensure_acid_tables()
+    live34.ensure_attention_tables()
+    live35.ensure_admin_tasks_table()
+    live35.seed_monday_task()
+    live37.update_monday_task_text()
+    live39.seed_probnik_return_task()
+    live41.ensure_weekly_report_tables()
+    live41.seed_current_trainers()
+    live48.ensure_metals_tables()
+    live48.register_metals_trainer()
+    live43.ensure_probnik_analysis_tables()
+    live44.enable_probnik_analysis_now()
+    live46.ensure_monthly_auto_report_table()
+    live50.seed_molar_mass_task()
+    live51.ensure_course_schedule_table()
+    live56.log_probnik_cabinet_audit()
+    live24.main()
