8b360ebcdac822279073aaed0059ed8946e6d4b8
mariyasedova08-beep <mariyasedova08@gmail.com>
2026-09-12 18:23:27 +0300

    Register payment delivery job safely at application build

---
+++ payment_delivery_hook.py
@@ -0,0 +1,25 @@
+"""Attach payment delivery jobs to the Telegram application without editing legacy main()."""
+from telegram.ext import ApplicationBuilder
+
+import payment_delivery
+
+_original_build = ApplicationBuilder.build
+_installed = False
+
+
+def _build_with_payment_delivery(self):
+    application = _original_build(self)
+    payment_delivery.register_jobs(application)
+    return application
+
+
+def install():
+    global _installed
+    if _installed:
+        return
+    ApplicationBuilder.build = _build_with_payment_delivery
+    _installed = True
+    print("Payment delivery application hook installed", flush=True)
+
+
+install()
