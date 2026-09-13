18e17a85d9985ce66d2d01c6d2ed982e09cdb44b
mariyasedova08-beep <mariyasedova08@gmail.com>
2026-09-13 13:02:28 +0300

    Load Sep 14 tutor reminder seed in production

--- run_bot_production.py
+++ run_bot_production.py
@@ -133,6 +133,7 @@ def main():
     import run_bot_live91  # noqa: F401
     import run_bot_live94  # noqa: F401
     import tutor_dashboard  # noqa: F401
+    import tutor_seed_20260914  # noqa: F401
 
     live90.live79.live24.main()
 
